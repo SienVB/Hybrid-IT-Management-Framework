3043bbbc36092c91cddc64c6e687ae2946f0a934
Sien Van Broekhoven <Sien.van-broekhoven@hpe.com>
2021-05-04 15:26:16 +0200

    fix

--- files/winrmADC.ps1
+++ files/winrmADC.ps1
@@ -3,6 +3,6 @@ Install-WindowsFeature RSAT-AD-Tools
 
 ## Add ADC VM to domain
 $Username = "stagesien\sien"
-$SecurePassword = ConvertTo-SecureString "6?3Wkv7mAk]aOsdR" -AsPlainText -Force
+$SecurePassword = ConvertTo-SecureString "XXXX" -AsPlainText -Force
 $credentials = New-Object System.Management.Automation.PSCredential($Username, $SecurePassword)
 Add-Computer -Credential (Get-Credential $credentials) -DomainName stagesien.com -Restart -Confirm:$false
--- files/winrmclient.ps1
+++ files/winrmclient.ps1
@@ -1,5 +1,5 @@
 ## Add ADC VM to domain
 $Username = "stagesien\sien"
-$SecurePassword = ConvertTo-SecureString "6?3Wkv7mAk]aOsdR" -AsPlainText -Force
+$SecurePassword = ConvertTo-SecureString "XXXX" -AsPlainText -Force
 $credentials = New-Object System.Management.Automation.PSCredential($Username, $SecurePassword)
 Add-Computer -Credential (Get-Credential $credentials) -DomainName stagesien.com -Restart -Confirm:$false
